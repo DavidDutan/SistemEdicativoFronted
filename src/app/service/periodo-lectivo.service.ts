f5b633f99be817c0083929f2cd9e05d51cb19778
MarcosPacheco19 <marcosxpa2020@outlook.com>
2023-07-24 09:25:03 -0500

    servicios en angular Nelson Ortega

--- src/app/service/periodo-lectivo.service.ts
+++ src/app/service/periodo-lectivo.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PeriodoLectivo } from 'src/domain/PeriodoLectivo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeriodoLectivoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  save (periodo: PeriodoLectivo): Observable<any[]> {
+      return this.http.post<any>('http://localhost:8080/usuario/registrar', periodo)
+  }
+
+  getAllEstudiantes(): Observable<any[]>{
+    return this.http.get<any>('http://localhost:8080/usuario/listar')
+  }
 }
