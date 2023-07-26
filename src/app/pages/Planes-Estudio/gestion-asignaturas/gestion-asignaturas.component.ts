7a0a2cf98e10d0e3fe853ea5c281326c3c98b41f
DavidDutan <daviddutan14@gmail.com>
2023-07-26 00:02:37 -0500

    Guardado Asignatura

--- src/app/pages/Planes-Estudio/gestion-asignaturas/gestion-asignaturas.component.ts
+++ src/app/pages/Planes-Estudio/gestion-asignaturas/gestion-asignaturas.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AsignaturaService } from 'src/app/service/asignatura.service';
+import { Asignatura } from 'src/domain/Asignatura';
+
 
 @Component({
   selector: 'app-gestion-asignaturas',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./gestion-asignaturas.component.scss']
 })
 export class GestionAsignaturasComponent {
+
+  lstEstudiantes = new Array()
+
+  dataSource: Asignatura[] = []; 
+
+  displayedColumns: string[] = ['Id', 'nombre', 'Creditos'];
+
+  asignatura: Asignatura = new Asignatura();
+
+  asignaturas:any;
+
+  constructor(private router: Router, private asignaturaService: AsignaturaService ) {}
+
+  ngOnInit(): void {
+    this.loadAsignatura()
+  }
+
+  loadAsignatura(){
+    this.asignaturas = this.asignaturaService.getAllAsignaturas()
+    console.log(this.asignatura)
+    this.asignaturas.subscribe((data: any) => {
+      console.log("data", data)
+      this.dataSource = data
+    })
+  }
+
+  guardar(){
+    console.log(this.asignatura)
+    this.asignaturaService.save(this.asignatura).subscribe(data =>{
+      console.log(data)
+      this.loadAsignatura()
+      this.limpiar()
+    })
+  }
+
+  limpiar(){
+
+
+    this.asignatura.asigNombre="";
+
+  }
+
+
 
 }
